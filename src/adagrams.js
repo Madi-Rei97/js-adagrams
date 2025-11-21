89d448688f4d89a048b702903a558fdc6c2adb62
Madi Rei <madison.nicole.rei@gmail.com>
2025-11-21 14:51:54 -0800

    creates letter pool and functionality for making the draw pile and drawing a hand of letters (wave 1)

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,52 @@
+const LETTER_POOL = {
+  'A': 9,
+  'B': 2,
+  'C': 2,
+  'D': 4,
+  'E': 12,
+  'F': 2,
+  'G': 3,
+  'H': 2,
+  'I': 9,
+  'J': 1,
+  'K': 1,
+  'L': 4,
+  'M': 2,
+  'N': 6,
+  'O': 8,
+  'P': 2,
+  'Q': 1,
+  'R': 6,
+  'S': 4,
+  'T': 6,
+  'U': 4,
+  'V': 2,
+  'W': 2,
+  'X': 1,
+  'Y': 2,
+  'Z': 1
+};
+
+const HAND_SIZE = 10;
+
+const makeDrawPile = () => {
+  let drawPile = [];
+
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    let lettersToAdd = Array(count).fill(letter);
+    drawPile.push(...lettersToAdd);
+  }
+  return drawPile;
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let newPile = makeDrawPile();
+  let hand = [];
+
+  for (let i = 0; i < HAND_SIZE; i++) {
+    hand.push(...newPile.splice(Math.floor(Math.random() * newPile.length), 1));
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
